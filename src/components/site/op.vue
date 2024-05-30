5cdb44483c679ae25fee685a7d678b49e750a932
Neal-L <tim85915@gmail.com>
2024-05-31 02:44:40 +0800

    Feat site component
    新增site頁面並增加各位置連結

---
+++ src/components/site/op.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-card height="245">
+    <v-img  cover src="file:///D:/4%E5%BA%AD/%E5%89%8D%E7%AB%AF%E7%B7%B4%E7%BF%92/html/web/image/Ushijima3.jpg"></v-img>
+    <v-btn :color="color" variant="text" class=text-1 rounded="xl" to="/op">{{ text_1 }}</v-btn>
+    <!-- <v-card-text class="text-1">{{ text_1 }}</v-card-text> -->
+  </v-card>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const text_1 = ref('舉球對角')
+const color = ref('white')
+</script>
+
+<style scoped>
+.text-1{
+  font-size: 20px;
+  font-weight: 600;
+  position: absolute;
+  z-index: 2;
+  right: 10px;
+  bottom: 10px;
+}
+</style>
--- src/layouts/site.vue
+++ src/layouts/site.vue
@@ -1,7 +1,23 @@
 <template>
-
+  <v-main class="pt-0">
+    <v-container>
+      <v-row  no-gutters>
+        <v-col md="12">
+          <ws />
+          <st />
+          <mb />
+          <op />
+          <lb />
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-main>
 </template>
 
 <script setup>
-
+import ws from '../components/site/ws.vue'
+import st from '../components/site/st.vue'
+import mb from '../components/site/mb.vue'
+import op from '../components/site/op.vue'
+import lb from '../components/site/lb.vue'
 </script>
